c4e062254d329732f49f477a428ce42e259373d1
Student astrum <student@astrum.domain>
2022-08-11 13:35:04 +0500

    added client page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,11 +10,11 @@ class PagesController < ApplicationController
   def client
     @application = Doorkeeper::Application.find_by(name: 'Web Client')
 
-    @application = {
-      name: @application.name,
-      client_id: @application.uid,
-      client_secret: @application.secret
-    }
+    # @application = {
+    #   name: @application.name,
+    #   client_id: @application.uid,
+    #   client_secret: @application.secret
+    # }
   end
   
 
